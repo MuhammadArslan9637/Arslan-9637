4528d9c9f0ef76e4ae0243d9566d29f05c7fbdfc
Muhammad Arslan 9637 <116496154+MuhammadArslan9637@users.noreply.github.com>
2022-10-28 15:53:31 +0500

    Add files via upload

---
+++ Machine Learning/Question5.1.py
@@ -0,0 +1,13 @@
+alien_color = "green"
+
+if alien_color == "green":
+    print("You just earned 5 points!")
+
+# 5.4
+
+alien_color = "green"
+
+if alien_color == "green":
+    print("You just earned 5 points!")
+else:
+    print("You just earned 10 points!")
